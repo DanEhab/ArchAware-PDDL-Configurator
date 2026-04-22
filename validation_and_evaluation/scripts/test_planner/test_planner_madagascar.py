3ba9da4ac828e78c036b2440904b01f85d745cd7
Daniel <danielehab05@gmail.com>
2026-04-22 15:10:10 +0200

    feat: ensure all planners collects the metrics needed for final analysis phase

--- validation_and_evaluation/scripts/test_planner/test_planner_madagascar.py
+++ validation_and_evaluation/scripts/test_planner/test_planner_madagascar.py
@@ -18,8 +18,8 @@ from pathlib import Path
 # Paths
 SCRIPT_DIR = Path(__file__).resolve().parent
 PROJECT_ROOT = SCRIPT_DIR.parent.parent.parent
-DOMAIN_FILE = PROJECT_ROOT / "benchmarks" / "visitall" / "domain.pddl"
-PROBLEM_FILE = PROJECT_ROOT / "benchmarks" / "visitall" / "instances" / "tiny-test.pddl"
+DOMAIN_FILE = PROJECT_ROOT / "benchmarks" / "ricochet-robots" / "domain.pddl"
+PROBLEM_FILE = PROJECT_ROOT / "benchmarks" / "ricochet-robots" / "instances" / "instance-01.pddl"
 
 def run_madagascar(domain_path: Path, problem_path: Path):
     benchmark_dir = domain_path.parent.parent.resolve()
